ed71e6c7064418ed871a0578ce7d7cb27cdb0eb8
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2025-10-23 15:24:35 +0800

    feat: express boilerplate
    Signed-off-by: Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>

--- pocket-server/src/index.ts
+++ pocket-server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
